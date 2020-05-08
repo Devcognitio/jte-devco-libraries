f4ed7f9776c166e5c62a35aa55c2b3fd0a507880
Carlos Acevedo <cacevedod@devco.com.co>
2020-05-08 12:00:11 -0500

    más parametros

--- pipeline_config.groovy
+++ pipeline_config.groovy
@@ -3,3 +3,16 @@ libraries{
     hola_mundo
     approve
 }
+
+application_environments{
+    merge = true
+    dev{
+        long_name = "Desarrollo"
+    }
+    qa{
+        long_name = "Certificacion"
+    }
+    prod{
+        long_name = "Production"
+    }
+}
