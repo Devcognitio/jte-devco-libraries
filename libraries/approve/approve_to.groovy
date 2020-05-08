b500f1db6616459b7bb07c0e51ad31dbc8816ba0
Carlos Acevedo <cacevedod@devco.com.co>
2020-05-08 16:43:08 -0500

    opcional por branch para approve

--- libraries/approve/approve_to.groovy
+++ libraries/approve/approve_to.groovy
@@ -1,16 +1,22 @@
 void call(app_env){
-    stage ("Deploy Approve to ${app_env.long_name}") {
-        if(app_env.long_name.equals('Production') ){
-            timeout(time:1, unit:'DAYS'){  
-                input message: 'Can you approve for IT ??', submitter: app_env.approversIT                
-            }
-            timeout(time:1, unit:'DAYS'){
-                input message: 'Can you approve for Bussiness ??', submitter: app_env.approversBussiness
-            }
-        }else{
-            timeout(time:1, unit:'HOURS'){
-                input message: 'Can you approve for IT ??', submitter: app_env.approversIT
+    
+    def branch_conditional = config.java_tool ?:
+            "s"
+            
+    if(branch_conditional == "s" || branch_conditional == $GIT_BRANCH){
+        stage ("Deploy Approve to ${app_env.long_name}") {
+            if(app_env.long_name.equals('Production') ){
+                timeout(time:1, unit:'DAYS'){  
+                    input message: 'Can you approve for IT ??', submitter: app_env.approversIT                
+                }
+                timeout(time:1, unit:'DAYS'){
+                    input message: 'Can you approve for Bussiness ??', submitter: app_env.approversBussiness
+                }
+            }else{
+                timeout(time:1, unit:'HOURS'){
+                    input message: 'Can you approve for IT ??', submitter: app_env.approversIT
+                }
             }
         }
-    }
+    }    
 }
