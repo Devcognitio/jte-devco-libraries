395b7631d38c7a8ee905a0d4c21341910ab4ede2
Carlos Acevedo <cacevedod@devco.com.co>
2020-05-08 16:19:07 -0500

    prueba usando una version de java

--- libraries/gradle/build.groovy
+++ libraries/gradle/build.groovy
@@ -1,18 +1,15 @@
 void call(){
-    def docker_image = config.docker_image ?:
-            "maven:3-alpine"
-
+    
     stage("build gradle"){
         node {
-            docker.image(docker_image).withRun(){ c ->
-                //unstash "workspace"
-
+            withEnv(["PATH=${tool 'java8'}/bin:$PATH"]) {
                 sh 'chmod +x gradlew'
                 sh './gradlew clean build -DskipTests=true'
                 archiveArtifacts artifacts: 'build/libs/**/*.jar'
-                
+            
                 stash name: 'workspace', includes: "**"
-            }            
+            }
+            
         }       
     }   
 }
