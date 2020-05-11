2cb0e82a17e5c105ce23755b633d2cdcfa1d5556
Carlos Acevedo <cacevedod@devco.com.co>
2020-05-11 15:49:19 -0500

    arreglos del env de git

--- libraries/approve/approve_to.groovy
+++ libraries/approve/approve_to.groovy
@@ -3,7 +3,7 @@ void call(app_env){
     def branch_conditional = config.branch_conditional ?:
             "s"
 
-    if(branch_conditional == "s" || branch_conditional == $GIT_BRANCH){
+    if(branch_conditional == "s" || branch_conditional == $env.BRANCH_NAME){
         stage ("Deploy Approve to ${app_env.long_name}") {
             echo "Branch: $env.BRANCH_NAME, conditional: $branch_conditional"
             if(app_env.long_name.equals('Production') ){
