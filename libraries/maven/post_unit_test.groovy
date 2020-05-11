caa2ff5b06486620fab21ce53681dd08e12b885c
Carlos Acevedo <cacevedod@devco.com.co>
2020-05-11 14:42:44 -0500

    corrijo error de las carpetas en los test

--- libraries/maven/post_unit_test.groovy
+++ libraries/maven/post_unit_test.groovy
@@ -1,7 +1,7 @@
 @AfterStep({ context.step.equals("unit_test") })
 void call(context){
     node{
-        junit 'surefire-reports/*.xml'
-        archiveArtifacts artifacts: 'site/jacoco-aggregate/*.xml'
+        junit 'target/surefire-reports/*.xml'
+        archiveArtifacts artifacts: 'target/site/jacoco-aggregate/*.xml'
     }    
 }
