8c98ee0b7720e614a15b1537ec6af90af8c46c83
Carlos Acevedo <cacevedod@devco.com.co>
2020-05-11 15:19:16 -0500

    Un paso personalizado para algunos proyectos

--- libraries/hola_mundo/deploy_to.groovy
+++ libraries/hola_mundo/deploy_to.groovy
@@ -1,9 +1,14 @@
 void call(app_env){
-    stage ("Deploy to ${app_env.long_name}") {
-        if(app_env.long_name.equals('Production') ){
-            println('Deploy a Produccion DONE...')
-        }else{
-            println('Este ambiente no requiere deploy')
+    def branch_conditional = config.branch_conditional ?:
+            "s"
+
+    if (branch_conditional == "s" || branch_conditional == $GIT_BRANCH){
+        stage ("Deploy to ${app_env.long_name}") {
+            if(app_env.long_name.equals('Production') ){
+                println('Deploy a Produccion DONE...')
+            }else{
+                println('Este ambiente no requiere deploy')
+            }
         }
     }
 }
